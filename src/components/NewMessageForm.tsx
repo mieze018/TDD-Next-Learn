885dd89c733a0bc84b882139d16000fba4fbf979
Ayu Nakata <mieze018@users.noreply.github.com>
2023-03-24 22:28:29 +0900

    ⛏  コンポーネントテストに沿った実装にする

--- src/components/NewMessageForm.tsx
+++ src/components/NewMessageForm.tsx
@@ -1,8 +1,19 @@
+import { useState } from 'react'
+
 export const NewMessageForm = () => {
+  const [message, setMessage] = useState('')
+  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setMessage(e.target.value)
+  const handleSend = () => setMessage('')
   return (
     <>
-      <input type="text" data-testid="messageText" />
-      <button type="button" data-testid="sendButton">
+      <input
+        type="text"
+        data-testid="messageText"
+        value={message}
+        onChange={handleTextChange}
+      />
+      <button type="button" data-testid="sendButton" onClick={handleSend}>
         送信
       </button>
     </>
